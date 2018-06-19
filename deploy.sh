5b5445f5dca3fad73c2f7d090e979acfd652c5b5
Tan Nguyen <tan.qh.nguyen@gmail.com>
2018-06-19 22:47:01 +0300

    Add letsencrypt

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 hugo
-ssh dokku@tannguyen.org apps:create tannguyen.org
+ssh dokku@tannguyen.org apps:create blog
+ssh dokku@tannguyen.org domains:add blog tannguyen.org
+ssh dokku@tannguyen.org letsencrypt blog
 touch public/.static
 cp -fv .dokku/* public
-tar c public | ssh dokku@tannguyen.org tar:in tannguyen.org
+tar c public | ssh dokku@tannguyen.org tar:in blog

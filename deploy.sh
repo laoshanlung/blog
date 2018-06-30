ca3f713929c9ba4e1ef1ab87b770aef20b78d79c
Tan Nguyen <tan.qh.nguyen@gmail.com>
2018-06-30 23:28:59 +0300

    Set email

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 hugo
 ssh dokku@tannguyen.org apps:create blog
 ssh dokku@tannguyen.org domains:add blog tannguyen.org
+ssh dokku@tannguyen.org config:set --no-restart blog DOKKU_LETSENCRYPT_EMAIL=tan.qh.nguyen@gmail.com
 ssh dokku@tannguyen.org letsencrypt blog
 touch public/.static
 cp -fv .dokku/* public
